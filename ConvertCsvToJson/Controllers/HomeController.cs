db3c4a65872fa53f4cb3f4b983be1211a56550da
tim.chen73 <tim.chen@titansoft.com.sg>
2022-04-25 17:50:27 +0800

    init

---
+++ ConvertCsvToJson/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using ConvertCsvToJson.Models;
+using Microsoft.AspNetCore.Mvc;
+using Aspose.Cells; 
+
+namespace ConvertCsvToJson.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public void ConvertToJson(string filePath)
+    {
+        var workbook = new Workbook(filePath);
+        workbook.Save("Output.json");
+    }
+}
---
+++ ConvertCsvToJson/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConvertCsvToJson.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
